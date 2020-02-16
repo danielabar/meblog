1066b5f98bc6582939ad289a2befb9db849b41dd
Daniela Baron <baron.daniela@gmail.com>
2020-02-16 11:22:29 -0500

    remove unused property

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 import React from "react"
 import styles from "./footer.module.css"
 
-export default ({ children }) => (
+export default () => (
   <footer className={styles.container}>
     <div className={styles.footer}>Footer copy TBD</div>
   </footer>
