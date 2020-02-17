deb5b0e2d98a285e1aa6a70139b9c0ea310044a4
Daniela Baron <baron.daniela@gmail.com>
2020-02-17 09:41:05 -0500

    add previous and next links to blog list template

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -1,16 +1,33 @@
 import React from "react"
+import { Link } from "gatsby"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import ArticleList from "../components/article-list"
 
-// TODO: Prev/Next links
 export default class BlogList extends React.Component {
   render() {
+    const { currentPage, numPages } = this.props.pageContext
+    const isFirst = currentPage === 1
+    const isLast = currentPage === numPages
+    const prevPage =
+      currentPage - 1 === 1 ? "/blog" : `/blog/${(currentPage - 1).toString()}`
+    const nextPage = `/blog/${(currentPage + 1).toString()}`
+
     return (
       <Layout>
         <ArticleList
           articles={this.props.data.allMarkdownRemark.edges}
         ></ArticleList>
+        {!isFirst && (
+          <Link to={prevPage} rel="prev">
+            ← Previous Page
+          </Link>
+        )}
+        {!isLast && (
+          <Link to={nextPage} rel="next">
+            Next Page →
+          </Link>
+        )}
       </Layout>
     )
   }
