0aab9b69c3ffed745d5e1d25604eed2cf41b91ae
Daniela Baron <baron.daniela@gmail.com>
2020-03-08 16:47:46 -0400

    style test on post page

--- src/templates/post.js
+++ src/templates/post.js
@@ -13,7 +13,7 @@ export default props => {
     <Layout>
       <SEO title={title} pathname={slug} />
       <div className={styles.container}>
-        <h1>{title}</h1>
+        <h1 className={styles.title}>{title}</h1>
         <div
           className={styles.content}
           dangerouslySetInnerHTML={{ __html: content }}
