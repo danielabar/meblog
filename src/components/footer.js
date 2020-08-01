cc1c65b6f8f2d6528bbeabb2d0c3f08de34abace
Daniela Baron <baron.daniela@gmail.com>
2020-08-01 17:34:50 -0400

    fix up some gatsby warnings for incorrect usage of Link element

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 import styles from "./footer.module.css"
 import { FaTwitter, FaGithub, FaCodepen, FaLinkedinIn } from "react-icons/fa"
 
@@ -16,18 +15,18 @@ const Footer = () => {
       <hr className={styles.sep} />
       <p className={styles.copy}>All materials © Daniela Baron 2020</p>
       <div className={styles.social}>
-        <Link to="https://twitter.com/DanielaMBaron">
+        <a href="https://twitter.com/DanielaMBaron">
           <FaTwitter size="1.7rem" color={iconColor} />
-        </Link>
-        <Link to="https://github.com/danielabar">
+        </a>
+        <a href="https://github.com/danielabar">
           <FaGithub size="1.7rem" color={iconColor} />
-        </Link>
-        <Link to="https://codepen.io/danielabar">
+        </a>
+        <a href="https://codepen.io/danielabar">
           <FaCodepen size="1.7rem" color={iconColor} />
-        </Link>
-        <Link to="https://www.linkedin.com/in/danielabaron/">
+        </a>
+        <a href="https://www.linkedin.com/in/danielabaron/">
           <FaLinkedinIn size="1.7rem" color={iconColor} />
-        </Link>
+        </a>
       </div>
     </footer>
   )
