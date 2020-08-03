8934b4526854885c2d684fd35748d93281dc73ab
Daniela Baron <baron.daniela@gmail.com>
2020-08-03 10:15:44 -0400

    fix viewport hook to work with SSR

--- src/hooks/useviewport.js
+++ src/hooks/useviewport.js
@@ -1,13 +1,17 @@
 import React from "react"
 
 const useViewport = () => {
-  const [width, setWidth] = React.useState(window.innerWidth)
+  const windowObj =
+    typeof window !== "undefined"
+      ? window
+      : { innerWidth: 960, addEventListener: () => "" }
+  const [width, setWidth] = React.useState(windowObj.innerWidth)
 
   React.useEffect(() => {
-    const handleWindowResize = () => setWidth(window.innerWidth)
-    window.addEventListener("resize", handleWindowResize)
-    return () => window.removeEventListener("resize", handleWindowResize)
-  }, [])
+    const handleWindowResize = () => setWidth(windowObj.innerWidth)
+    windowObj.addEventListener("resize", handleWindowResize)
+    return () => windowObj.removeEventListener("resize", handleWindowResize)
+  }, [windowObj])
 
   // Return the width so we can use it in our components
   return { width }
