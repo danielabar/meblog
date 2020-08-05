a491d611e62db4327e4259ac68b9e23ff4220d83
Daniela Baron <baron.daniela@gmail.com>
2020-08-05 17:24:21 -0400

    footer icon hover effect

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,29 +3,28 @@ import styles from "./footer.module.css"
 import { FaTwitter, FaGithub, FaCodepen, FaLinkedinIn } from "react-icons/fa"
 
 const Footer = () => {
-  const iconColor =
-    typeof window !== "undefined"
-      ? window
-          .getComputedStyle(document.documentElement)
-          .getPropertyValue("--base-font-color")
-      : ""
-
   return (
     <footer className={styles.container}>
       <hr className={styles.sep} />
       <p className={styles.copy}>All materials © Daniela Baron 2020</p>
       <div className={styles.social}>
-        <a href="https://twitter.com/DanielaMBaron">
-          <FaTwitter size="1.7rem" color={iconColor} />
+        <a
+          href="https://twitter.com/DanielaMBaron"
+          className={styles.footerIcon}
+        >
+          <FaTwitter size="1.7rem" />
         </a>
-        <a href="https://github.com/danielabar">
-          <FaGithub size="1.7rem" color={iconColor} />
+        <a href="https://github.com/danielabar" className={styles.footerIcon}>
+          <FaGithub size="1.7rem" />
         </a>
-        <a href="https://codepen.io/danielabar">
-          <FaCodepen size="1.7rem" color={iconColor} />
+        <a href="https://codepen.io/danielabar" className={styles.footerIcon}>
+          <FaCodepen size="1.7rem" />
         </a>
-        <a href="https://www.linkedin.com/in/danielabaron/">
-          <FaLinkedinIn size="1.7rem" color={iconColor} />
+        <a
+          href="https://www.linkedin.com/in/danielabaron/"
+          className={styles.footerIcon}
+        >
+          <FaLinkedinIn size="1.7rem" />
         </a>
       </div>
     </footer>
