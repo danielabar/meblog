e3ebac10a9225de93186de0542cf7f7fe6eb2083
Daniela Baron <baron.daniela@gmail.com>
2020-08-15 17:11:16 -0400

    add featured image to each post

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,11 +1,16 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 import SEO from "../components/SEO"
 import Layout from "../components/layout"
 import AllLink from "../components/all-link"
 import styles from "./post.module.css"
 
-// props.pageContext object contains context from gatsby-node.js createPages
+// props.pageContext contains context from gatsby-node.js createPages
+// props.data contains result from query object defined at bottom of this component - needed for featured image
 export default props => {
+  const featuredImgFluid =
+    props.data.markdownRemark.frontmatter.featuredImage.childImageSharp.fluid
   const content = props.pageContext.content
   const title = props.pageContext.title
   const description = props.pageContext.description || props.pageContext.title
@@ -21,6 +26,7 @@ export default props => {
       />
       <div className={styles.container}>
         <h1 className={styles.title}>{title}</h1>
+        <Img fluid={featuredImgFluid} className={styles.featureImage} />
         <div
           className={styles.content}
           dangerouslySetInnerHTML={{ __html: content }}
@@ -30,3 +36,26 @@ export default props => {
     </Layout>
   )
 }
+
+// query results available to component in props.data
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        description
+        featuredImage {
+          childImageSharp {
+            fluid(maxWidth: 800) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+      fields {
+        slug
+      }
+    }
+  }
+`
