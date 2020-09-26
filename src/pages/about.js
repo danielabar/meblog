ad4e9562264cb83a0c3662665e7feab961cd3c97
Daniela Baron <baron.daniela@gmail.com>
2020-09-26 17:50:04 -0400

    add content for about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,6 +7,8 @@ export default () => (
   <Layout>
     <SEO title="About" pathname="/about" />
     <div className={styles.container}>
+      <h1 className={styles.header}>About</h1>
+
       <p>
         Hi there, thanks for finding my blog. I'm a fullstack software engineer
         with over 15 years experience delivering consumer facing and large scale
@@ -14,11 +16,60 @@ export default () => (
         technologies. Experienced in Agile methodologies including Scrum and XP.
         Strong focus on test driven development, refactoring, automated testing
         and continuous integration. Passionate about ongoing education to learn
-        new technologies, tools and languages.
+        new technologies, tools and languages. I mostly blog abut programming
+        related topics but occasionally write about other topics such as
+        personal finance, and health and fitness.
       </p>
+
+      <h2 className={styles.subheader}>My Story</h2>
+
       <p className={styles.para}>
-        I mostly blog abut programming related topics but occasionally write
-        about other topics such as personal finance, and health and fitness.
+        I discovered software development during first year at university.
+        Students were not required to pick a major until second year so first
+        year was all about discovery. One of the courses I picked was an
+        introduction to computers. It covered several areas including software,
+        hardware, networking, programming, and theoretical computer science. The
+        programming section was taught using{" "}
+        <a href="https://en.wikipedia.org/wiki/Karel_(programming_language)">
+          Karel the Robot
+        </a>
+        . This is an ideal introductory language because the syntax is very
+        simple and the scope of what Karel can do is limited. It's a great way
+        to learn the fundamental concepts of programming including variables,
+        loops, control flow and functions, while not getting caught up in
+        complexities of compilers, data types, memory management, and garbage
+        collection (plenty of time for that later). I enjoyed the programming
+        section of this course so much that I would stay up all night perfecting
+        my homework assignments and even adding extra features just for fun. At
+        some point during first year I realized people actually get paid to do
+        this and decided to make computer science my major.
+      </p>
+
+      <p className={styles.para}>
+        After graduation I landed a software development job at a big box
+        retailer, working on their e-commerce platform. It was at this point
+        that I realized my formal education had been long on theory but short on
+        the practical. To my great surprise, no one needed me to determine loop
+        invariants, compute a proof by induction or perform a{" "}
+        <a href="https://en.wikipedia.org/wiki/Big_O_notation">Big O</a>{" "}
+        complexity analysis. What was required was the ability to work on a
+        large multidisciplinary team, use version control, work with databases,
+        keep a large codebase organized, and reproduce a pdf design from
+        Photoshop in HTML and CSS with pixel perfection on all browsers prior to
+        box model standardization. Fortunately I had several wonderful mentors
+        to guide me through the on-the-job learning process.
+      </p>
+
+      <p className={styles.para}>
+        Since then I've worked at a number of Product, Project, and SaaS based
+        companies using a variety of languages and frameworks including
+        Java/Spring/Hibernate, JavaScript (Node.js, Angular, Ember, React), Go,
+        Python and Rails. I decided to take up blogging after many years working
+        in this industry. During this time I've learned a lot and decided to
+        share it on this blog. Although I've done an enormous amount of
+        technical writing during my career, the kind of prose style of writing
+        required for a blog is a nice challenge to improve my creative writing
+        skills. I hope others will find my posts helpful.
       </p>
     </div>
   </Layout>
