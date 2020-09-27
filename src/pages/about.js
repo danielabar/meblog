b2832be3eb0dfc94a5acf5c61d336d2c33059cef
Daniela Baron <baron.daniela@gmail.com>
2020-09-27 11:13:28 -0400

    refine about copy and styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,16 +9,15 @@ export default () => (
     <div className={styles.container}>
       <h1 className={styles.header}>About</h1>
 
-      <p>
+      <p className={styles.para}>
         Hi there, thanks for finding my blog. I'm a fullstack software engineer
         with over 15 years experience delivering consumer facing and large scale
-        enterprise applications. Well versed in database, server and client side
-        technologies. Experienced in Agile methodologies including Scrum and XP.
-        Strong focus on test driven development, refactoring, automated testing
-        and continuous integration. Passionate about ongoing education to learn
-        new technologies, tools and languages. I mostly blog abut programming
-        related topics but occasionally write about other topics such as
-        personal finance, and health and fitness.
+        enterprise applications. I have experience working with a variety of
+        database, server and client side technologies. I've been especially
+        focused on writing clean, easy to understand code, documentation, test
+        driven development, refactoring, and continuous integration. I'm
+        passionate about ongoing education to learn new technologies, tools and
+        languages.
       </p>
 
       <h2 className={styles.subheader}>My Story</h2>
@@ -27,10 +26,16 @@ export default () => (
         I discovered software development during first year at university.
         Students were not required to pick a major until second year so first
         year was all about discovery. One of the courses I picked was an
-        introduction to computers. It covered several areas including software,
-        hardware, networking, programming, and theoretical computer science. The
-        programming section was taught using{" "}
-        <a href="https://en.wikipedia.org/wiki/Karel_(programming_language)">
+        introduction to computers. It covered several areas including hardware,
+        networking, programming, and theoretical computer science.
+      </p>
+
+      <p className={styles.para}>
+        The programming section of this course was taught using{" "}
+        <a
+          className={styles.link}
+          href="https://en.wikipedia.org/wiki/Karel_(programming_language)"
+        >
           Karel the Robot
         </a>
         . This is an ideal introductory language because the syntax is very
@@ -38,11 +43,11 @@ export default () => (
         to learn the fundamental concepts of programming including variables,
         loops, control flow and functions, while not getting caught up in
         complexities of compilers, data types, memory management, and garbage
-        collection (plenty of time for that later). I enjoyed the programming
+        collection (plenty of time for that later!). I enjoyed the programming
         section of this course so much that I would stay up all night perfecting
         my homework assignments and even adding extra features just for fun. At
-        some point during first year I realized people actually get paid to do
-        this and decided to make computer science my major.
+        some point during first year I learned that people actually get paid to
+        do this and decided to make computer science my major.
       </p>
 
       <p className={styles.para}>
@@ -51,17 +56,22 @@ export default () => (
         that I realized my formal education had been long on theory but short on
         the practical. To my great surprise, no one needed me to determine loop
         invariants, compute a proof by induction or perform a{" "}
-        <a href="https://en.wikipedia.org/wiki/Big_O_notation">Big O</a>{" "}
+        <a
+          className={styles.link}
+          href="https://en.wikipedia.org/wiki/Big_O_notation"
+        >
+          Big O
+        </a>{" "}
         complexity analysis. What was required was the ability to work on a
         large multidisciplinary team, use version control, work with databases,
-        keep a large codebase organized, and reproduce a pdf design from
-        Photoshop in HTML and CSS with pixel perfection on all browsers prior to
-        box model standardization. Fortunately I had several wonderful mentors
-        to guide me through the on-the-job learning process.
+        keep a large codebase organized, and reproduce pdf design comps in HTML
+        and CSS with pixel perfection on all browsers prior to box model
+        standardization. Fortunately I had several wonderful mentors to guide me
+        through the on-the-job learning process.
       </p>
 
       <p className={styles.para}>
-        Since then I've worked at a number of Product, Project, and SaaS based
+        Since then I've worked at a number of product, project, and SaaS based
         companies using a variety of languages and frameworks including
         Java/Spring/Hibernate, JavaScript (Node.js, Angular, Ember, React), Go,
         Python and Rails. I decided to take up blogging after many years working
