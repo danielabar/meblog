9a945bfd2400e212c8765653be4a1f043545e1c1
Daniela Baron <baron.daniela@gmail.com>
2021-05-24 12:14:12 -0400

    fetch search results from server

--- src/components/search-input.js
+++ src/components/search-input.js
@@ -16,12 +16,26 @@ const SearchInput = () => {
   }
 
   const search = debounce((text) => {
-    console.log(text)
-    // TODO: If have at least 3 characters, fetch(`process.env.SEARCH_URL?q=${text}`)
-    // when get results, navigate programmatically to new page /search
-    // see this example: https://codesandbox.io/embed/github/jlengstorf/egghead-gatsby-link/tree/master/?module=%2Fsrc%2Fpages%2Fsign-up.js
-    // also see nice search input UI: https://egghead.io/lessons/gatsby-navigate-to-a-new-page-programmatically-in-gatsby
+    if (text.length > 3) {
+      fetchSearchResults(text)
+    }
   }, 300);
+
+  // when get results, navigate programmatically to new page /search
+  // see this example: https://codesandbox.io/embed/github/jlengstorf/egghead-gatsby-link/tree/master/?module=%2Fsrc%2Fpages%2Fsign-up.js
+  // also see nice search input UI: https://egghead.io/lessons/gatsby-navigate-to-a-new-page-programmatically-in-gatsby
+  async function fetchSearchResults(query) {
+    const response = await fetch(`${process.env.SEARCH_URL}?q=${query}`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
+    if (response.ok) {
+      const json = await response.json();
+      console.dir(json);
+    }
+  }
 
   return (
     <input type="text"
