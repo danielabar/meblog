b2d461a2eae36620785cd9a62a48cc49363da494
Daniela Baron <baron.daniela@gmail.com>
2021-07-11 16:08:25 -0400

    fix typo in intro

--- src/components/intro.js
+++ src/components/intro.js
@@ -4,7 +4,7 @@ import styles from "./intro.module.css"
 export default () => (
   <section className={styles.container}>
     I'm a software engineer based in Toronto, Canada. My interests include
-    fullstack develoment, test automation, personal finance, listening to
+    fullstack development, test automation, personal finance, listening to
     podcasts, reading, building side projects, health and fitness, and always
     learning new things.
   </section>
