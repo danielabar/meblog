097eb7d9fe45c53bb47434a8a5da2779c34fe201
Daniela Baron <baron.daniela@gmail.com>
2021-08-18 20:06:19 -0400

    test article and article list

---
+++ src/components/article-list.spec.js
@@ -0,0 +1,42 @@
+import React from "react"
+import renderer from "react-test-renderer"
+
+import ArticleList from "./article-list"
+
+describe("AllLink", () => {
+  it("renders correctly", () => {
+    const testData = [
+      {
+        node: {
+          id: '123abc',
+          fields: {
+            slug: '/blog/some-article-1'
+          },
+          frontmatter: {
+            title: 'Some Article 1',
+            category: 'Rails',
+            date: 'August 2021'
+          },
+          excerpt: 'First sentence of article 1 blah blah blah...'
+        }
+      },
+      {
+        node: {
+          id: '456def',
+          fields: {
+            slug: '/blog/some-article-2'
+          },
+          frontmatter: {
+            title: 'Some Article 2',
+            category: 'Docker',
+            date: 'September 2021'
+          },
+          excerpt: 'First sentence of article 2 blah blah blah...'
+        }
+      }
+    ]
+
+    const testRenderer = renderer.create(<ArticleList articles={testData}/>);
+    expect(testRenderer.toJSON()).toMatchSnapshot()
+  })
+})
---
+++ src/components/article.spec.js
@@ -0,0 +1,18 @@
+import React from "react"
+import renderer from "react-test-renderer"
+
+import Article from "./article"
+
+describe("Article", () => {
+  it("renders correctly", () => {
+    const testRenderer = renderer.create(<Article
+                                            key="123abc"
+                                            id="123abc"
+                                            to="/blog/some-article"
+                                            title="Some Article"
+                                            category="Rails"
+                                            date="August 2021"
+                                            excerpt="First sentence of the article blah blah blah..."/>);
+    expect(testRenderer.toJSON()).toMatchSnapshot()
+  })
+})
