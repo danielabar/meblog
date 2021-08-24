e3536a092e13c2afc8c6333cf31ce96a00fdd34f
Daniela Baron <baron.daniela@gmail.com>
2021-08-24 11:14:41 -0400

    test pagination component

---
+++ src/components/pagination.spec.js
@@ -0,0 +1,82 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from "react"
+import { render, fireEvent, screen } from "@testing-library/react"
+import "@testing-library/jest-dom"
+
+import Pagination from "./pagination"
+
+// First page:   { "isFirst": true, "prevPage": "/blog/0", "isLast": false, "nextPage": "/blog/2" }
+// Middle page:  { "isFirst": false, "prevPage": "/blog", "isLast": false, "nextPage": "/blog/3" }
+// Last page:    { "isFirst": false, "prevPage": "/blog/7", "isLast": true, "nextPage": "/blog/9" }
+describe("Pagination", () => {
+  it("renders first page", () => {
+    const container = render(
+      <Pagination
+        isFirst={true}
+        prevPage={"/blog/0"}
+        isLast={false}
+        nextPage={"/blog/2"}
+      />
+    )
+    expect(container).toMatchSnapshot()
+
+    expect(screen.queryByTestId("previous-enabled")).not.toBeInTheDocument()
+    expect(screen.queryByTestId("previous-disabled")).toBeInTheDocument()
+    expect(screen.queryByTestId("next-enabled")).toBeInTheDocument()
+    expect(screen.queryByTestId("next-disabled")).not.toBeInTheDocument()
+  })
+
+  it("renders middle page", () => {
+    const container = render(
+      <Pagination
+        isFirst={false}
+        prevPage={"/blog/3"}
+        isLast={false}
+        nextPage={"/blog/4"}
+      />
+    )
+    expect(container).toMatchSnapshot()
+
+    expect(screen.queryByTestId("previous-enabled")).toBeInTheDocument()
+    expect(screen.queryByTestId("previous-disabled")).not.toBeInTheDocument()
+    expect(screen.queryByTestId("next-enabled")).toBeInTheDocument()
+    expect(screen.queryByTestId("next-disabled")).not.toBeInTheDocument()
+  })
+
+  it("renders last page", () => {
+    const container = render(
+      <Pagination
+        isFirst={false}
+        prevPage={"/blog/7"}
+        isLast={true}
+        nextPage={"/blog/9"}
+      />
+    )
+    expect(container).toMatchSnapshot()
+
+    expect(screen.queryByTestId("previous-enabled")).toBeInTheDocument()
+    expect(screen.queryByTestId("previous-disabled")).not.toBeInTheDocument()
+    expect(screen.queryByTestId("next-enabled")).not.toBeInTheDocument()
+    expect(screen.queryByTestId("next-disabled")).toBeInTheDocument()
+  })
+
+  it("renders a single page", () => {
+    const container = render(
+      <Pagination
+        isFirst={true}
+        prevPage={"/blog/0"}
+        isLast={true}
+        nextPage={"/blog/2"}
+      />
+    )
+    expect(container).toMatchSnapshot()
+
+    expect(screen.queryByTestId("previous-enabled")).not.toBeInTheDocument()
+    expect(screen.queryByTestId("previous-disabled")).toBeInTheDocument()
+    expect(screen.queryByTestId("next-enabled")).not.toBeInTheDocument()
+    expect(screen.queryByTestId("next-disabled")).toBeInTheDocument()
+  })
+})
