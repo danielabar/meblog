a77a67e4e6003bcf855efd6e9f56a918449703b3
Daniela Baron <baron.daniela@gmail.com>
2021-09-11 17:32:08 -0400

    import font on post page

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
+import "@fontsource/fira-code"
 import SEO from "../components/SEO"
 import Layout from "../components/layout"
 import AllLink from "../components/all-link"
