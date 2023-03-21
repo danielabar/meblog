975be41d4c98a9211dd9748c3574ff66c7e53582
Daniela Baron <baron.daniela@gmail.com>
2023-03-21 09:44:28 -0400

    update test deps, config, and snapshots

--- jest.config.js
+++ jest.config.js
@@ -6,17 +6,19 @@ module.exports = {
   moduleNameMapper: {
     ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
-    "^gatsby-page-utils/(.*)$": `gatsby-page-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
     "\\.worker.js": "<rootDir>/__mocks__/workerMock.js",
     "@fontsource/*": "<rootDir>/__mocks__/font-mock.js",
     "^@reach/router(.*)": "<rootDir>/node_modules/@gatsbyjs/reach-router$1",
   },
   testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
-  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+  transformIgnorePatterns: [`node_modules/(?!(gatsby|gatsby-script|gatsby-link)/)`],
   globals: {
     __PATH_PREFIX__: ``,
   },
-  testURL: `http://localhost`,
+  testEnvironmentOptions: {
+    url: `http://localhost`,
+  },
   setupFiles: [`<rootDir>/loadershim.js`, "dotenv/config"],
+  setupFilesAfterEnv: ["<rootDir>/setup-test-env.js"],
   clearMocks: true,
 }
