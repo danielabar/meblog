7fc002d0bb0d8bdfddc44350e5a1b8ea9535e1ba
Daniela Baron <baron.daniela@gmail.com>
2024-02-08 17:17:58 -0500

    update about page content

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,76 +12,72 @@ const About = () => (
       <h1 className={styles.header}>About</h1>
 
       <p className={styles.para}>
-        Hi there, thanks for finding my blog. I'm a fullstack software engineer
-        with over 15 years experience delivering consumer facing and large scale
-        enterprise applications. I have experience working with a variety of
-        database, server and client side technologies. I've been especially
-        focused on writing clean, easy to understand code, documentation, test
-        driven development, refactoring, and continuous integration. I'm
-        passionate about ongoing education to learn new technologies, tools and
-        languages.
-      </p>
-
-      <h2 className={styles.subheader}>My Story</h2>
-
-      <p className={styles.para}>
-        I discovered software development during first year at university.
-        Students were not required to pick a major until second year so first
-        year was all about discovery. One of the courses I picked was an
-        introduction to computers. It covered several areas including hardware,
-        networking, programming, and theoretical computer science.
+        Welcome to my programming blog! I'm thrilled you've stumbled upon it.
+        With over two decades in software development, I've worked across many
+        industries and technology stacks, predominantly focusing on web
+        applications in projects, products, and SaaS solutions. My journey began
+        with Java, transitioning seamlessly into full-stack JavaScript, with
+        brief sojourns into Python and GoLang before finding my professional
+        haven in Ruby on Rails nearly five years ago. It's in this environment
+        that I've found not only happiness but also peak productivity.
       </p>
 
       <p className={styles.para}>
-        The programming section of this course was taught using{" "}
-        <a
-          className={styles.link}
-          href="https://en.wikipedia.org/wiki/Karel_(programming_language)"
-        >
-          Karel the Robot
-        </a>
-        . This is an ideal introductory language because the syntax is very
-        simple and the scope of what Karel can do is limited. It's a great way
-        to learn the fundamental concepts of programming including variables,
-        loops, control flow and functions, while not getting caught up in
-        complexities of compilers, data types, memory management, and garbage
-        collection (plenty of time for that later!). I enjoyed the programming
-        section of this course so much that I would stay up all night perfecting
-        my homework assignments and even adding extra features just for fun. At
-        some point during first year I learned that people actually get paid to
-        do this and decided to make computer science my major.
+        But let's rewind a bit. My fascination with computer science began
+        during my first year at university. An introductory course, covering
+        everything from hardware to programming, introduced me to the world of
+        coding through the Karel programming language. I was captivated by the
+        elegance of programming, spending countless hours perfecting assignments
+        and even adding extra features just for fun. Little did I know that this
+        newfound passion would become my lifelong career.
       </p>
 
       <p className={styles.para}>
-        After graduation I landed a software development job at a big box
-        retailer, working on their e-commerce platform. It was at this point
-        that I realized my formal education had been long on theory but short on
-        the practical. To my great surprise, no one needed me to determine loop
-        invariants, compute a proof by induction or perform a{" "}
-        <a
-          className={styles.link}
-          href="https://en.wikipedia.org/wiki/Big_O_notation"
-        >
-          Big O
-        </a>{" "}
-        complexity analysis. What was required was the ability to work on a
-        large multidisciplinary team, use version control, work with databases,
-        keep a large codebase organized, and reproduce pdf design comps in HTML
-        and CSS with pixel perfection on all browsers prior to box model
-        standardization. Fortunately I had several wonderful mentors to guide me
-        through the on-the-job learning process.
+        Upon graduation, I landed a role working on a major retailer's
+        e-commerce platform. It was here that the disparity between academic
+        theory and practical application first became apparent. While my formal
+        education had equipped me with a solid foundation in computer science
+        theory, the demands of the job were markedly different. Instead of loop
+        invariants and Big-O complexity analysis, I found myself navigating the
+        intricacies of version control, database management, and the art of
+        translating design comps into pixel-perfect HTML and CSS (before box
+        model standardization!). Fortunately I had several wonderful mentors to
+        guide me through the on-the-job learning process.
       </p>
 
       <p className={styles.para}>
-        Since then I've worked at a number of product, project, and SaaS based
-        companies using a variety of languages and frameworks including
-        Java/Spring/Hibernate, JavaScript (Node.js, Angular, Ember, React), Go,
-        Python and Rails. I decided to take up blogging after many years working
-        in this industry. During this time I've learned a lot and decided to
-        share it on this blog. Although I've done an enormous amount of
-        technical writing during my career, the kind of prose style of writing
-        required for a blog is a nice challenge to improve my creative writing
-        skills. I hope others will find my posts helpful.
+        Since then, my career has been defined by a commitment to bridging the
+        gap between theory and practice. As I transitioned from a junior
+        developer to a leadership role as a Staff Engineer, I've made it my
+        mission to advocate for practices that prioritize both efficiency and
+        long-term maintainability. From fostering clear communication with
+        product stakeholders and quality assurance to championing automated
+        testing and CI/CD pipelines, I'm dedicated to nurturing an environment
+        where every engineer thrives.
+      </p>
+
+      <p className={styles.para}>
+        Moreover, as an experienced practitioner, I actively mentor and guide
+        fellow engineers, believing strongly in the power of knowledge sharing
+        to elevate the entire team. I'm deeply invested in fostering
+        reproducible local environments, advocating for architectural
+        improvements, and consistently striving for performance optimization and
+        observability best practices.
+      </p>
+
+      <p className={styles.para}>
+        Beyond the confines of my organization, I'm passionate about sharing
+        insights and best practices with the broader tech community through
+        blogging. This platform not only serves as a means of personal growth
+        but also as a conduit for sharing programming stories, problem solving
+        techniques, learning strategies, and career development.
+      </p>
+
+      <p className={styles.para}>
+        So whether you're a seasoned developer or just starting your journey, I
+        invite you to join me as we explore the ever-evolving landscape of
+        software engineering together. Here's to learning, growing, and building
+        better software, one blog post at a time.
       </p>
       <AllLink marginTop="30px" />
     </div>
