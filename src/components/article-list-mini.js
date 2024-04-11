a249ed5bba1ef1c344835ccb1e6f0e4612daefc4
Daniela Baron <baron.daniela@gmail.com>
2024-04-11 10:20:09 -0400

    make link to articles in mini component

--- src/components/article-list-mini.js
+++ src/components/article-list-mini.js
@@ -1,14 +1,17 @@
 import React from "react"
+import { Link } from "gatsby"
 import * as styles from "./article-list-mini.module.css"
 
 const ArticleList = props => (
   <section className={styles.container} data-testid="article-list-mini">
     <h2 className={styles.header}>{props.title}</h2>
     {props.articles.map(({ node }) => (
-      <div className={styles.article}>
-        <h3 className={styles.articleTitle}>{node.frontmatter.title}</h3>
-        <div className={styles.articleDate}>{node.frontmatter.date}</div>
-      </div>
+      <Link to={node.fields.slug} key={node.id}>
+        <div className={styles.article}>
+          <span className={styles.articleTitle}>{node.frontmatter.title}</span>
+          <div className={styles.articleDate}>{node.frontmatter.date}</div>
+        </div>
+      </Link>
     ))}
   </section>
 )
